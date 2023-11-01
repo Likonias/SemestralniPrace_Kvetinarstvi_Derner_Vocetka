552736b867dc3c268d0abfaf2963fddab97a5cdc
Likonias <lisoan123@gmail.com>
2023-11-01 10:55:54 +0100

    Add project files.

---
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        OracleConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connection = GetConnection();
+            connection.Open();
+
+        }
+        public static OracleConnection GetConnection()
+        {
+            string connectionString = "User Id=st00000;" +
+                "Password=heslo;" +
+                "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=fei-sql3.upceucebny.cz)(PORT=1521))(CONNECT_DATA=(SID=BDAS)(SERVER=DEDICATED)))";
+            return new OracleConnection(connectionString);
+        }
+
+        private string read(OracleDataReader reader, int columnIndex)
+        {
+            return reader.IsDBNull(columnIndex) ? "..." : reader.GetString(columnIndex);
+        }
+    }
+}
