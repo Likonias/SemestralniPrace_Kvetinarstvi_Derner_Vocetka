175f301c7356137220fcbca1dead6a043366f6aa
Likonias <lisoan123@gmail.com>
2023-11-02 16:16:41 +0100

    update třídy

---
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models
+{
+    internal class Class1
+    {
+    }
+}
---
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models
+{
+    internal class Employee : User
+    {
+        
+    }
+}
