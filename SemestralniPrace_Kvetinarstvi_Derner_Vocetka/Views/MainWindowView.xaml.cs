56c8e8e84bda00a562cdc1094f2279b3831e946a
Likonias <lisoan123@gmail.com>
2023-11-08 19:39:03 +0100

    login?

--- SemestralniPrace_Kvetinarstvi_Derner_Vocetka/Views/MainWindowView.xaml.cs
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/Views/MainWindowView.xaml.cs
@@ -12,12 +12,20 @@ namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Views
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
         public MainWindow()
         {
             InitializeComponent();
 
         }
-       
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            SemestralniPrace_Kvetinarstvi_Derner_Vocetka.ViewModels.LoginViewModel login =
+               new SemestralniPrace_Kvetinarstvi_Derner_Vocetka.ViewModels.LoginViewModel();
+            
+            this.DataContext = login;
+        }
+
     }
 }
