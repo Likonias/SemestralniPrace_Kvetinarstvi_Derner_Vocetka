d514320234caf90b2e5c67b36ed88d35a957b926
Likonias <lisoan123@gmail.com>
2023-11-14 16:17:50 +0100

    update

---
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Utils;
+using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.ViewModels;
+using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Views;
+using System.Windows;
+
+namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            Navigation navigation = new Navigation();
+
+            navigation.CurrentViewModel = new MainViewModel(navigation);
+
+            MainWindow = new MainWindow()
+            {
+                DataContext = navigation.CurrentViewModel
+            };
+
+
+            base.OnStartup(e);
+        }
+    }
+}
