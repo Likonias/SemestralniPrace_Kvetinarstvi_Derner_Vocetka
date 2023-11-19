429bd64e953f9e059791c2bad3ab81e5dfef2459
Likonias <lisoan123@gmail.com>
2023-11-19 08:45:52 +0100

    Malý update a zkouška DataTable

--- SemestralniPrace_Kvetinarstvi_Derner_Vocetka/ViewModels/MainViewModel.cs
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Utils;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +15,27 @@ namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.ViewModels
 
         public ICommand NavigateLoginCommand { get; }
         public ICommand NavigateRegisterCommand { get; }
+        public ObservableCollection<DataRow> DataGridItems { get; }
 
         public MainViewModel(Navigation navigation)
         {
             NavigateLoginCommand = new NavigateCommand<LoginViewModel>(navigation, () => new LoginViewModel(navigation));
             NavigateRegisterCommand = new NavigateCommand<RegisterViewModel>(navigation, () => new RegisterViewModel(navigation));
+
+            DataGridItems = new ObservableCollection<DataRow>();
+
+            // Fetch table data using OracleDbUtil
+            OracleDbUtil dbUtil = new OracleDbUtil();
+            DataTable tableData = dbUtil.ExecuteQuery("SELECT * FROM zakaznici");
+
+            if (tableData != null)
+            {
+                foreach (DataRow row in tableData.Rows)
+                {
+                    DataGridItems.Add(row);
+                }
+            }
+
         }
     }
 }
