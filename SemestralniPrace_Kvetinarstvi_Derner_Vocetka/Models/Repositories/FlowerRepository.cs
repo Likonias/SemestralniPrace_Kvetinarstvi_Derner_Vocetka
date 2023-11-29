f7f90ad24f5e8519f14a3a82c9c60188eb11f96f
Jan Vocetka <hopi6570@gmail.com>
2023-11-29 15:24:10 +0100

    FlowerRepository.cs zkouska opravy

--- SemestralniPrace_Kvetinarstvi_Derner_Vocetka/Models/Repositories/FlowerRepository.cs
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/Models/Repositories/FlowerRepository.cs
@@ -1,109 +1,143 @@
-//using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models.Enums;
-//using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models.Interfaces;
-//using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Utils;
-//using System;
-//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
-//using System.Data;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models.Enums;
+using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models.Interfaces;
+using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//TODO flowerRepository
-
-//namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models.Repositories
-//{
-//    public class FlowerRepository : IRepository<Flower>
-//    {
-//        public ObservableCollection<Flower> Flowers { get; set; }
-//        private OracleDbUtil dbUtil;
+namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models.Repositories
+{
+    public class FlowerRepository : IRepository<Flower>
+    {
+        public ObservableCollection<Flower> Flowers { get; set; }
+        private OracleDbUtil dbUtil;
         
-//        public FlowerRepository()
-//        {
-//            Flowers = new ObservableCollection<Flower>();
-//            dbUtil = new OracleDbUtil();
-//        }
+        public FlowerRepository()
+        {
+            Flowers = new ObservableCollection<Flower>();
+            dbUtil = new OracleDbUtil();
+        }
         
-//        public async Task<Flower> GetById(Int32 id)
-//        { 
-//            string command = $"SELECT * FROM kvetiny WHERE ID_KVETINA = {id}";
-//            DataTable dataTable = await dbUtil.ExecuteQueryAsync(command);
+        public async Task<Flower> GetById(Int32 id)
+        { 
+            string command = $"SELECT * FROM kvetiny " +
+                             $"LEFT JOIN zbozi ON kvetiny.id_zbozi = zbozi.id_zbozi " +
+                             $"WHERE ID_KVETINA = {id}";
             
-//            if (dataTable.Rows.Count == 0)
-//                return null;
+            DataTable dataTable = await dbUtil.ExecuteQueryAsync(command);
             
-//            var row = dataTable.Rows[0];
-//            var flower = new Flower(
-//                Convert.ToInt32(row["ID_KVETINA"]),
-//                row["NAZEV"].ToString(),
-//                Convert.ToInt32(row["CENA"]),
-//                Convert.ToInt32(row["POCET"])
-//            );
+            if (dataTable.Rows.Count == 0)
+                return null;
             
-//            return (Flower)Convert.ChangeType(flower, typeof(Flower));
-//        }
+            var row = dataTable.Rows[0];
+            var flower = new Flower(
+                Convert.ToInt32(row["ID_ZBOZI"]),
+                row["NAZEV"].ToString(),
+                Convert.ToDouble(row["CENA"]),
+                Convert.ToByte(row["TYP"]),
+                Convert.ToInt32(row["SKLAD"]),
+                null,
+                Convert.ToInt32(row["ID_KVETINA"]),
+                MapDatabaseValueToEnum(row["STAV"].ToString()),
+                Convert.ToInt32(row["STARI"])
+            );
+            
+            return (Flower)Convert.ChangeType(flower, typeof(Flower));
+        }
 
-//        public async Task GetAll()
-//        {
-//            string command = "SELECT * FROM kvetiny";
-//            DataTable dataTable = await dbUtil.ExecuteQueryAsync(command);
+        public async Task GetAll()
+        {
+            string command = $"SELECT * FROM kvetiny " +
+                             $"LEFT JOIN zbozi ON kvetiny.id_zbozi = zbozi.id_zbozi ";
+            DataTable dataTable = await dbUtil.ExecuteQueryAsync(command);
             
-//            foreach (DataRow row in dataTable.Rows)
-//            {
-//                var flower = new Flower(
-//                    Convert.ToInt32(row["ID_KVETINA"]),
-//                    row["NAZEV"].ToString(),
-//                    Convert.ToInt32(row["CENA"]),
-//                    Convert.ToInt32(row["POCET"])
-//                );
-//                Flowers.Add(flower);
-//            }
-//        }
+            foreach (DataRow row in dataTable.Rows)
+            {
+                var flower = new Flower(
+                    Convert.ToInt32(row["ID_ZBOZI"]),
+                    row["NAZEV"].ToString(),
+                    Convert.ToDouble(row["CENA"]),
+                    Convert.ToByte(row["TYP"]),
+                    Convert.ToInt32(row["SKLAD"]),
+                    null,
+                    Convert.ToInt32(row["ID_KVETINA"]),
+                    MapDatabaseValueToEnum(row["STAV"].ToString()),
+                    Convert.ToInt32(row["STARI"])
+                );
+                Flowers.Add(flower);
+            }
+        }
 
-//        public async Task Add(Flower entity)
-//        {
-//            var parameters = new Dictionary<string, object>
-//            {
-//                {"STAV", entity.State.ToString()},
-//                {"STARI", entity.Age}
-//            };
-//            await dbUtil.ExecuteStoredProcedureAsync("addkvetina", parameters);
-//        }
+        public async Task Add(Flower entity)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"STAV", entity.State.ToString()},
+                {"STARI", entity.Age}
+            };
+            await dbUtil.ExecuteStoredProcedureAsync("addkvetina", parameters);
+        }
 
-//        public async Task Update(Flower entity)
-//        {
-//            var parameters = new Dictionary<string, object>
-//            {
-//                {"ID_KVETINA", entity.IdFlower},
-//                {"STAV", entity.State.ToString()},
-//                {"STARI", entity.Age}
-//            };
-//            await dbUtil.ExecuteStoredProcedureAsync("updatekvetina", parameters);
-//        }
+        public async Task Update(Flower entity)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"ID_KVETINA", entity.IdFlower},
+                {"STAV", entity.State.ToString()},
+                {"STARI", entity.Age}
+            };
+            await dbUtil.ExecuteStoredProcedureAsync("updatekvetina", parameters);
+        }
 
-//        public async Task Delete(Flower entity)
-//        {
-//            var parameters = new Dictionary<string, object>
-//            {
-//                {"ID_KVETINA", entity.IdFlower}
-//            };
-//            await dbUtil.ExecuteStoredProcedureAsync("deletekvetina", parameters);
-//        }
+        public async Task Delete(Flower entity)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"ID_KVETINA", entity.IdFlower}
+            };
+            await dbUtil.ExecuteStoredProcedureAsync("deletekvetina", parameters);
+        }
 
-//        public async Task<DataTable> ConvertToDataTable()
-//        {
-//            await GetAll();
-//            DataTable dataTable = new DataTable();
+        public async Task<DataTable> ConvertToDataTable()
+        {
+            await GetAll();
+            DataTable dataTable = new DataTable();
             
-//            dataTable.Columns.Add("State", typeof(FlowerStateEnum));
-//            dataTable.Columns.Add("Age", typeof(int));
-            
-//            foreach (var flower in Flowers)
-//            {
-//                dataTable.Rows.Add(flower.State, flower.Age);
-//            }
-            
-//            return dataTable;
-//        }
-//    }
-//}
+            dataTable.Columns.Add("State", typeof(FlowerStateEnum));
+            dataTable.Columns.Add("Age", typeof(int));
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                var flower = new Flower(
+                    Convert.ToInt32(row["ID_ZBOZI"]),
+                    row["NAZEV"].ToString(),
+                    Convert.ToDouble(row["CENA"]),
+                    Convert.ToByte(row["TYP"]),
+                    Convert.ToInt32(row["SKLAD"]),
+                    null,
+                    Convert.ToInt32(row["ID_KVETINA"]),
+                    MapDatabaseValueToEnum(row["STAV"].ToString()),
+                    Convert.ToInt32(row["STARI"])
+                );
+
+                dataTable.Rows.Add(flower.State, flower.Age);
+            }
+
+            return dataTable;
+        }
+        
+        private FlowerStateEnum MapDatabaseValueToEnum(string databaseValue)
+        {
+            if (Enum.TryParse<FlowerStateEnum>(databaseValue, out var flowerStateEnum))
+            {
+                return flowerStateEnum;
+            }
+            // Handle the case where the database value doesn't match any enum value
+            throw new ArgumentException($"Invalid FlowerStateEnum value: {databaseValue}");
+        }
+    }
+}
