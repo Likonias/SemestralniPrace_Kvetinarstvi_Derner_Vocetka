8ed7def9438bca862daab1d85424fc870cf75214
Likonias <lisoan123@gmail.com>
2023-12-04 09:42:45 +0100

    account

--- SemestralniPrace_Kvetinarstvi_Derner_Vocetka/ViewModels/AccountViewModel.cs
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/ViewModels/AccountViewModel.cs
@@ -10,10 +10,15 @@ namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.ViewModels
     public class AccountViewModel : ViewModelBase
     {
         private AccountStore accountStore;
-
+        private string name;
+        public string Name { get { return name; } set { name = value; OnPropertyChanged(Name); } }
+        private string email;
+        public string Email { get { return email; } set { email = value; OnPropertyChanged(Email); } }
         public AccountViewModel(AccountStore accountStore)
         {
             this.accountStore = accountStore;
+            Name = accountStore.CurrentAccount.FirstName + " " + accountStore.CurrentAccount.LastName;
+            Email = accountStore.CurrentAccount.Email;
         }
     }
 }
