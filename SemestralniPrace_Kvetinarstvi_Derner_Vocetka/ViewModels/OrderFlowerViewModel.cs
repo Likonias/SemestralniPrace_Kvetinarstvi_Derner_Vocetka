633c2de3416da56b45b70378ef98ada4f11fc6e2
Jan Vocetka <st67125@upce.cz>
2023-12-07 19:04:04 +0100

    Push smth

--- SemestralniPrace_Kvetinarstvi_Derner_Vocetka/ViewModels/OrderFlowerViewModel.cs
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/ViewModels/OrderFlowerViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Navigation;
+using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Navigation.Stores;
 using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Utils;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +13,32 @@ namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.ViewModels
     public class OrderFlowerViewModel : ViewModelBase
     {
         public RelayCommand BtnClose { get; }
-        public OrderFlowerViewModel(INavigationService navigationService)
+        private DataTable tableData;
+        private OracleDbUtil dbUtil;
+        public DataTable TableData
+        {
+            get { return tableData; }
+            set
+            {
+                tableData = value;
+                OnPropertyChanged(nameof(TableData));
+            }
+        }
+        public OrderFlowerViewModel(OrderStore orderStore, INavigationService navigationService)
         {
             NavigationService = navigationService;
             BtnClose = new RelayCommand(BtnCloseClicked);
+            dbUtil = new OracleDbUtil();
+            tableData = new DataTable();
+            InitializeTableData();
         }
+
+        private async Task InitializeTableData()
+        {
+            TableData = new DataTable();
+            TableData = await dbUtil.ExecuteGetGoodsFunctionAsync()
+        }
+
         private void BtnCloseClicked()
         {
             NavigationService.Navigate();
