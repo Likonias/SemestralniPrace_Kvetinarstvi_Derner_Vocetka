ca199aa3356828f8a7bbe34e2c2e3a101ffa299b
Likonias <lisoan123@gmail.com>
2023-12-08 01:15:33 +0100

    order form almost done

--- SemestralniPrace_Kvetinarstvi_Derner_Vocetka/ViewModels/OrderFormViewModel.cs
+++ SemestralniPrace_Kvetinarstvi_Derner_Vocetka/ViewModels/OrderFormViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models.Enums;
+﻿using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models;
+using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Models.Enums;
 using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Navigation;
 using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Navigation.Stores;
 using SemestralniPrace_Kvetinarstvi_Derner_Vocetka.Utils;
@@ -13,6 +14,7 @@ namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.ViewModels
 {
     public class OrderFormViewModel : ViewModelBase
     {
+        private OracleDbUtil dbUtil;
         private OrderStore orderStore; 
         private DataTable tableData;
         private INavigationService closeNavigationService;
@@ -29,19 +31,51 @@ namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.ViewModels
         }
         public List<OccasionTypeEnum> OccasionComboBoxItems { get; }
         public List<BillingTypeEnum> BillingComboBoxItems { get; }
-        public List<string> GoodsComboBoxItems { get; }
-        public List<int> GoodsId { get; }
-        public List<int> GoodsCount { get; }
-
+        public List<string> GoodsComboBoxItems { get; set; }
+        public List<int> GoodsId { get; set; }
+        public List<int> GoodsCount { get; set; }
+        private List<int> GoodsIdInOrder { get; set; }
+        private List<int> GoodsCountInOrder { get; set; }
+        public List<string> DeliveryComboBoxItems { get; }
+        public List<string> DeliveryCompanyComboBoxItems { get; }
         public RelayCommand BtnOk { get; }
         public RelayCommand BtnCancel { get; }
-
+        public RelayCommand BtnAddToOrder { get; }
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set
+            {
+                errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        private bool isDeliverySelected;
+        public bool IsDeliverySelected
+        {
+            get { return isDeliverySelected; }
+            set
+            {
+                isDeliverySelected = value;
+                OnPropertyChanged("IsDeliverySelected");
+            }
+        }
+        private DataTable dataTable;
         public OrderFormViewModel(OrderStore orderStore, INavigationService closeNavigationService, INavigationService createOrderFlower, INavigationService createOrderOther)
         {
             this.orderStore = orderStore;
             this.closeNavigationService = closeNavigationService;
             this.createOrderFlower = createOrderFlower;
             this.createOrderOther = createOrderOther;
+            dbUtil = new OracleDbUtil();
+            TableData = new DataTable();
+            
+            dataTable = new DataTable();
+
+            dataTable.Columns.Add("ID_ZBOZI");
+            dataTable.Columns.Add("NAZEV");
+            dataTable.Columns.Add("SKLAD");
 
             OccasionComboBoxItems = Enum.GetValues(typeof(OccasionTypeEnum)).Cast<OccasionTypeEnum>().ToList();
             BillingComboBoxItems = Enum.GetValues(typeof(BillingTypeEnum)).Cast<BillingTypeEnum>().ToList();
@@ -49,37 +83,163 @@ namespace SemestralniPrace_Kvetinarstvi_Derner_Vocetka.ViewModels
             GoodsComboBoxItems = new List<string>();
             GoodsId = new List<int>();
             GoodsCount = new List<int>();
-
+            GoodsIdInOrder = new List<int>();
+            GoodsCountInOrder = new List<int>();
+            DeliveryComboBoxItems = new List<string>
+            {
+                "In Person",
+                "Delivery Company"
+            };
+            IsDeliverySelected = false;
+            DeliveryCompanyComboBoxItems = new List<string>
+            {
+                "PPL",
+                "DHL",
+                "CeskaPosta"
+            };
+            BtnAddToOrder = new RelayCommand(BtnAddToOrderClicked);
             BtnOk = new RelayCommand(BtnOkClicked);
             BtnCancel = new RelayCommand(BtnCancelClicked);
 
-            LoadGoodsFromDatabase(); // Call method to load goods from SQL
+            LoadGoodsFromDatabase();
+        }
+
+        private void BtnAddToOrderClicked()
+        {
+            if (int.TryParse(Count, out int enteredCount))
+            {
+                int selectedGoodsIndex = GoodsComboBoxItems.IndexOf(SelectedGoods);
+                if (selectedGoodsIndex >= 0 && selectedGoodsIndex < GoodsCount.Count)
+                {
+                    if (enteredCount <= GoodsCount[selectedGoodsIndex])
+                    {
+                        GoodsIdInOrder.Add(GoodsId[selectedGoodsIndex]);
+                        GoodsCountInOrder.Add(GoodsCount[selectedGoodsIndex]);
+
+                        GoodsCount[selectedGoodsIndex] -= enteredCount;
+
+                        dataTable.Rows.Add(
+                            GoodsId[selectedGoodsIndex],
+                            GoodsComboBoxItems[selectedGoodsIndex],
+                            enteredCount
+                        );
+                        TableData = dataTable;
+                        
+                    }
+                }
+            }
         }
 
         private void BtnCancelClicked()
         {
-            throw new NotImplementedException();
+            closeNavigationService.Navigate();
         }
 
         private void BtnOkClicked()
         {
+            //TODO použití procedury
             throw new NotImplementedException();
         }
 
-        private void LoadGoodsFromDatabase()
+        private async void LoadGoodsFromDatabase()
         {
-            //// Placeholder SQL query
-            //string query = "SELECT Id, Name FROM GoodsTable"; // Modify to your table name and structure
+            // Placeholder SQL query
+            string query = "SELECT ID_ZBOZI, NAZEV, SKLAD FROM ZBOZI"; // Modify to your table name and structure
 
-            //// Execute the query and fetch data
-            //// Assuming you have a database connection or a service to execute queries
-            //// Replace this with your actual database handling code
-            //var goodsData = YourDatabaseService.ExecuteQuery(query);
+            DataTable dt = await dbUtil.ExecuteQueryAsync(query);
+            
+            //GoodsComboBoxItems = dt.AsEnumerable().Select(row => row.Field<string>("NAZEV")).ToList();
+            //GoodsId = dt.AsEnumerable().Select(row => Convert.ToInt32(row.Field<decimal>("ID_ZBOZI"))).ToList();
+            //GoodsCount = dt.AsEnumerable().Select(row => Convert.ToInt32(row.Field<decimal>("SKLAD"))).ToList();
+            foreach (DataRow row in dt.Rows)
+            {
+                GoodsId.Add(Convert.ToInt32(row["ID_ZBOZI"]));
+                GoodsComboBoxItems.Add(row["NAZEV"].ToString());
+                GoodsCount.Add(Convert.ToInt32(row["SKLAD"]));
+            }
 
-            //GoodsComboBoxItems = goodsData.Select(item => item["Name"].ToString()).ToList();
-            //GoodsId = goodsData.Select(item => Convert.ToInt32(item["Id"])).ToList();
-            //GoodsCount = new List<int>(GoodsId.Count); // Initializing an empty list for counts
         }
 
+        private OccasionTypeEnum _selectedOccasion;
+        public OccasionTypeEnum SelectedOccasion
+        {
+            get => _selectedOccasion;
+            set
+            {
+                _selectedOccasion = value;
+                OnPropertyChanged(nameof(SelectedOccasion));
+            }
+        }
+
+        private BillingTypeEnum _selectedBilling;
+        public BillingTypeEnum SelectedBilling
+        {
+            get => _selectedBilling;
+            set
+            {
+                _selectedBilling = value;
+                OnPropertyChanged(nameof(SelectedBilling));
+            }
+        }
+
+        private string _selectedDelivery;
+        public string SelectedDelivery
+        {
+            get => _selectedDelivery;
+            set
+            {
+                _selectedDelivery = value;
+                if(value == "Delivery Company")
+                {
+                    IsDeliverySelected = true;
+                }
+                OnPropertyChanged(nameof(SelectedDelivery));
+            }
+        }
+
+        private string _selectedGoods;
+        public string SelectedGoods
+        {
+            get => _selectedGoods;
+            set
+            {
+                _selectedGoods = value;
+                OnPropertyChanged(nameof(SelectedGoods));
+            }
+        }
+
+        private string _count;
+        public string Count
+        {
+            get => _count;
+            set 
+            { 
+                _count = value;
+                if (!IsCountLowerThanSelectedGoodsCount() && Count != "")
+                {
+                    ErrorMessage = "Not enough stock!";
+                }
+                else
+                {
+                    ErrorMessage = "";
+                }
+                OnPropertyChanged(nameof(Count));
+            }
+        }
+
+        public bool IsCountLowerThanSelectedGoodsCount()
+        {
+            if (int.TryParse(Count, out int enteredCount))
+            {
+                // Assuming GoodsCount and GoodsComboBoxItems are aligned by index
+                int selectedGoodsIndex = GoodsComboBoxItems.IndexOf(SelectedGoods);
+                if (selectedGoodsIndex >= 0 && selectedGoodsIndex < GoodsCount.Count)
+                {
+                    int selectedGoodsCount = GoodsCount[selectedGoodsIndex];
+                    return enteredCount < selectedGoodsCount;
+                }
+            }
+            return false;
+        }
     }
 }
